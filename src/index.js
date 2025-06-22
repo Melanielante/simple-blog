1772e1d8be83612472335b407da995d4bbcb84c4
Melanie Akinyi <kleinmelanie04@gmail.com>
2025-06-22 15:59:47 +0300

    fetching post content from the server

---
+++ src/index.js
@@ -0,0 +1,68 @@
+// waiting for the DOM content tto be loaded
+
+function main() {
+    displayPosts();
+    addNewPostListener();
+}
+document.addEventListener("DOMContentLoaded", main);
+
+//fetching all the blog posts in the post-list container
+
+function displayPosts() {
+    fetch("http://localhost:3000/posts")
+    .then(res => res.json())
+    .then(posts => {
+        const postList = document.getElementById("post-list");
+
+
+        posts.forEach(post => {
+            const postItem = document.createElement("div");
+            postItem.textContent = post.title;
+            postItem.classList.add("post-item");
+            postItem.dataset.id = post.id;
+
+            //adding click event to show post details
+            postItem.addEventListener("click", () => handlePostClick(post.id));
+
+            postList.appendChild(postItem);
+        });
+    })
+}
+
+// showing post details when the title is clicked on the sidebar
+
+function handlePostClick(id) {
+    fetch(`http://localhost:3000/posts/${id}`)
+    .then(res => res.json())
+    .then(post => {
+        const postDetail = document.getElementById("post-detail");
+
+        //clearing old content
+        postDetail.innerHTML = "";
+
+        //creating elements for the post
+
+        //title
+        const title = document.createElement("h2");
+        title.textContent = post.title;
+
+        //author
+        const author = document.createElement("p");
+        author.textContent = `By: ${post.author}`;
+
+        //image
+        const image = document.createElement("img");
+        image.src = post.image || "";
+        image.alt = post.title;
+        image.style.maxWidth = "100%";
+
+        //content
+        const content = document.createElement("p");
+        content.textContent = post.content;
+
+        //appending all created elements to post-detail
+        postDetail.append(title, author, image,content);
+
+
+    })
+}
